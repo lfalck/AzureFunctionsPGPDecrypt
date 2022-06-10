e8e66f136c1e9460e042ad75d7ccdf0821344ef7
Ludvig Falck <lfalck@gmail.com>
2022-06-10 12:37:34 +0200

    Added support for verification.

--- AzureFunctionsPGPDecrypt/PGPDecryptAndVerify.cs
+++ AzureFunctionsPGPDecrypt/PGPDecryptAndVerify.cs
@@ -12,27 +12,36 @@ using Org.BouncyCastle.Bcpg.OpenPgp;
 
 namespace AzureFunctionsPGPDecrypt
 {
-    public static class PGPDecrypt
+    public static class PGPDecryptAndVerify
     {
-        [FunctionName(nameof(PGPDecrypt))]
+        [FunctionName(nameof(PGPDecryptAndVerify))]
         public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req, ILogger log)
         {
-            log.LogInformation($"C# HTTP trigger function {nameof(PGPDecrypt)} processed a request.");
+            log.LogInformation($"C# HTTP trigger function {nameof(PGPDecryptAndVerify)} processed a request.");
 
             string privateKeyBase64 = Environment.GetEnvironmentVariable("pgp-private-key");
             string passPhrase = Environment.GetEnvironmentVariable("pgp-passphrase");
+            string publicKeyVerifyBase64 = Environment.GetEnvironmentVariable("pgp-public-key-verify");
 
             if (string.IsNullOrEmpty(privateKeyBase64))
             {
                 return new BadRequestObjectResult($"Please add a base64 encoded private key to an environment variable called pgp-private-key");
             }
 
+            if (string.IsNullOrEmpty(publicKeyVerifyBase64))
+            {
+                return new BadRequestObjectResult($"Please add a base64 encoded public key to an environment variable called pgp-public-key-verify");
+            }
+
             byte[] privateKeyBytes = Convert.FromBase64String(privateKeyBase64);
             string privateKey = Encoding.UTF8.GetString(privateKeyBytes);
 
+            byte[] publicKeyVerifyBytes = Convert.FromBase64String(publicKeyVerifyBase64);
+            string publicKeyVerify = Encoding.UTF8.GetString(publicKeyVerifyBytes);
+
             try
             {
-                Stream decryptedData = await DecryptAsync(req.Body, privateKey, passPhrase);
+                Stream decryptedData = await DecryptAndVerifyAsync(req.Body, privateKey, publicKeyVerify, passPhrase);
                 return new OkObjectResult(decryptedData);
             }
             catch (PgpException pgpException)
@@ -41,7 +50,7 @@ namespace AzureFunctionsPGPDecrypt
             }
         }
 
-        private static async Task<Stream> DecryptAsync(Stream inputStream, string privateKey, string passPhrase)
+        private static async Task<Stream> DecryptAndVerifyAsync(Stream inputStream, string privateKey, string publicKeyVerify, string passPhrase)
         {
             using (PGP pgp = new PGP())
             {
@@ -49,8 +58,9 @@ namespace AzureFunctionsPGPDecrypt
 
                 using (inputStream)
                 using (Stream privateKeyStream = privateKey.ToStream())
+                using (Stream publicKeyVerifyStream = publicKeyVerify.ToStream())
                 {
-                    await pgp.DecryptStreamAsync(inputStream, outputStream, privateKeyStream, passPhrase);
+                    await pgp.DecryptStreamAndVerifyAsync(inputStream, outputStream, publicKeyVerifyStream, privateKeyStream, passPhrase);
                     outputStream.Seek(0, SeekOrigin.Begin);
                     return outputStream;
                 }
